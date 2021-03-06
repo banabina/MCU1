c54c4413adff4f4791493bb04485d994f7c3d7eb
banabina <banabina0012@gmail.com>
2021-03-06 21:39:55 +0900

    Write base address macros

--- stm32f4xx_drivers/drivers/Inc/stm32f446xx.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f446xx.h
@@ -8,6 +8,14 @@
 #ifndef STM32F446XX_H_
 #define STM32F446XX_H_
 
+/*
+ * base addresses of Flash and SRAM memories
+ */
+#define FLASH_BASEADDR				0x08000000U		// Main memory
+#define SRAM1_BASEADDR				0x20000000U		// 112 KB = 0x1C000
+#define SRAM2_BASEADDR				0x2001C000U
+#define ROM							0x1FFF0000U		// system memory
+#define SRAM 						SRAM1_BASEADDR
 
 
 #endif /* STM32F446XX_H_ */
